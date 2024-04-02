d6046f1654ac0519f60f5ba79e07f97f44c79af6
webency <webency@mail.ru>
2024-04-02 23:33:31 +0300

    expore jpa repository

--- src/main/java/ru/spmi/lessonsjpa/Application.java
+++ src/main/java/ru/spmi/lessonsjpa/Application.java
@@ -1,7 +1,11 @@
 package ru.spmi.lessonsjpa;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +14,35 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+		return args -> {
+			Student james = new Student("James", "Bond", "james007@gmail.com", 21);
+			Student anna = new Student("Anna", "Fortran", "anna2003@yandex.ru", 20);
+
+			System.out.println("Добавили студентов James и Anna");
+			studentRepository.saveAll(List.of(james, anna));
+
+			System.out.println("Количество студентов: ");
+			System.out.println(studentRepository.count());
+
+			studentRepository.findById(2L).ifPresentOrElse(
+					System.out::println,
+					() -> System.out.println("Student with ID 2 not found"));
+
+			studentRepository.findById(3L).ifPresentOrElse(
+					System.out::println,
+					() -> System.out.println("Student with ID 2 not found"));
+
+			System.out.println("Получаем всех студентов");
+			List<Student> students = studentRepository.findAll();
+			students.forEach(System.out::println);
+
+			System.out.println("Удаляем студента James");
+			studentRepository.deleteById(1L);
+
+			System.out.println("Количество студентов: ");
+			System.out.println(studentRepository.count());
+		};
+	}
 }
--- src/main/java/ru/spmi/lessonsjpa/Student.java
+++ src/main/java/ru/spmi/lessonsjpa/Student.java
@@ -56,8 +56,7 @@ public class Student {
     public Student() {
     }
 
-    public Student(Long id, String firstName, String lastName, String email, Integer age) {
-        this.id = id;
+    public Student(String firstName, String lastName, String email, Integer age) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
